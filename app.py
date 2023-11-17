76280442b6debfbc45f03c2a8d25a04ea6afc192
SOLKIONA <solkiona@gmail.com>
2023-11-17 05:00:04 +0100

    Elibrary Project

--- app.py
+++ app.py
@@ -23,9 +23,6 @@ with app.app_context():
     db.create_all()
 
 
-
-
-
 @app.route('/')
 def index():
     if 'user_id' in session:
@@ -42,38 +39,88 @@ def dashboard():
         user_id = session['user_id']
         user = User.query.get(user_id)
         admin_status = request.args.get('admin_status')
-        return render_template('dashboard.html', admin_status=admin_status,user=user)
+        books = Book.query.all()
+        print(books)
+        
+        # user = User.query.get_or_404(user_id)
+        # book = Book.query.get_or_404(book_id)
+        
+        return render_template('dashboard.html', admin_status=admin_status,user=user, books=books)
     else:
         return redirect(url_for('login.login'))
 
-@app.route('/base/')
-def base():
-    return render_template('base.html')
+@app.route('/base/<int:book_id>')
+def base(book_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        admin_status = request.args.get('admin_status')
+        
+        book = Book.query.get_or_404(book_id) 
+        return render_template('base.html', book=book)
+    return redirect('/')
 
 @app.route('/blog/')
 def blog():
     return render_template('blog.html')
 
+@app.route('/create')
+def create():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get(user_id)
+        admin_status = request.args.get('admin_status')
+        return render_template('create.html', admin_status=admin_status,user=user)
+    
+    
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
 @app.route('/favorite')
 def favorite():
-    fav = Book.query.get_or_404(2)
-    if fav.favorite == True:
-        fav.favorite = False
-        print('true')
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get_or_404(user_id)
+        favBooks = user.favorite_books
+        
+        return render_template('favorite.html', books=favBooks, user=user)
+        
+@app.route('/favorite/<int:book_id>')
+def toggle_favorite(book_id):
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.get_or_404(user_id)
+        book = Book.query.get_or_404(book_id)
+        print('got here')
+        if book in user.favorite_books:
+            user.favorite_books.remove(book)
+            print('removed from favorite')
+        else:
+            user.favorite_books.append(book)
+            print("added to favorites")
         db.session.commit()
-        print(Book.query.get_or_404(2).favorite)
-        return "No longer favorite"
-    
-    else:
-        fav.favorite = True
-        db.session.commit()
-        print(Book.query.get_or_404(2).favorite)
-        return "back to favorite"
-    
+        return ''
+        
  
 
-
+@app.route('/trigger_delay')
+def trigger_delay():
+    q = request.args.get('q')
+    print('content of  q is: ',    q)
+    if q.isspace() or len(q) <= 3 or q == '' or q == ' ':
+        return ''
+    
+    if  (not q.isspace())  and q != '' and (len(q) != 0):
+        print('q is :', q)
+        results = Book.query.filter(Book.Description.ilike(f"%{q}%")).limit(5).all()
+        if results:
+            return render_template('results.html', results = results)
+        else:
+            return "<h1 style='color: rgba(233, 226, 226, 0.5)'> NO RESULTS FOUND </h1>"
+    else:
+        return "<h1 style='color: rgba(233, 226, 226, 0.5)'> NO RESULTS FOUND </h1>"
+    
 @app.route('/fileupload/' , methods=['GET', 'POST'])
 def fileupload():
     if request.method == 'POST':
@@ -97,22 +144,24 @@ def fileupload():
         
         bookTitle = request.form['bookTitle']   
         bookAuthor = request.form['bookAuthor'] 
-        yearPublished = request.form['yearPublished']
         description = request.form['description'] 
         coverImage = os.path.relpath(os.path.join(upload_folder, filenameX).replace('\\', '/'), upload_folder)
         filepath = os.path.relpath(os.path.join(upload_folder, filenameY).replace('\\', '/'), upload_folder)
         
-        new_book = Book(bookTitle=bookTitle, bookAuthor=bookAuthor, yearPublished=yearPublished, coverImage=coverImage, Description=description, filepath=filepath)
+        new_book = Book(bookTitle=bookTitle, bookAuthor=bookAuthor,
+        coverImage=coverImage, Description=description, filepath=filepath)
         book_status = new_book
         db.session.add(new_book)
         db.session.commit()
         
-        return redirect(url_for('books'))
+        return redirect(url_for('dashboard'))
     
     return render_template('create.html')
         
         
-    
+
+
+
 
 @app.route('/books/')
 def books():
@@ -122,6 +171,15 @@ def books():
         book_status = Book.query.all()
         return render_template('books.html', user_id=user_id , book_status=book_status)
     return render_template('books.html')
+
+# @app.route('/deleteuser')
+# def deleteuser():
+#     user_to_delete = User.query.get_or_404(1)
+#     db.session.delete(user_to_delete)
+#     db.session.commit()
+    
+#     return 'User Deleted'
+
 
 @app.route('/download/<filename>')
 def download_file(filename):
@@ -141,13 +199,14 @@ def addbooks():
         
 
         
-@app.post('/<int:book_id>/delete/')
+@app.post('/delete/<int:book_id>')
 def delete(book_id):
     book = Book.query.get_or_404(book_id)
     db.session.delete(book)
     db.session.commit()
     
-    return render_template('books.html')
+    
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,18 +1,35 @@
 
 from db import db
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship 
 
 
+#Association Table for Favorite Books 
+favorite_books = db.Table(
+    'favorite_books', db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True)
+)
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     bookTitle = db.Column(db.String(100), nullable=False)
     bookAuthor = db.Column(db.String(100), nullable=False)
-    favorite = db.Column(db.Boolean, default=False)
+    # favorite = db.Column(db.Boolean, default=False)
     coverImage = db.Column(db.String(100), nullable=False)
     Description = db.Column(db.String(1000), nullable=False)
     filepath = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    
+    
+    #Relationship with User
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id', nullable=False))
+    user = relationship('User', back_populates='books')
+    
+    #Define a relationship with the favorite_books association table 
+    favorites = relationship('User', secondary=favorite_books, back_populates='favorite_books')
+    
     
     def __repr__(self):
         return f'<Book {self.bookTitle}>'
@@ -25,6 +42,10 @@ class User(db.Model):
     admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     
+    books = relationship('Book', back_populates='user', cascade='all, delete-orphan')
     
-    def __repr_(self):
+    favorite_books = relationship('Book', secondary=favorite_books, back_populates='favorites')
+    
+    
+    def __repr__(self):
         return f'<User {self.username}>'
